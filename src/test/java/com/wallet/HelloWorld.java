4c44da6eefd8083ad14936be3b748f237dcceef9
wso2 <wso2@rafaels.local>
2022-03-07 12:24:38 -0300

    Configuracoes iniciais

---
+++ src/test/java/com/wallet/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.wallet;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class HelloWorld {
+	
+	@Test
+	public void testHelloWorld() {
+		
+		assertEquals(1,1);
+	}
+
+}
